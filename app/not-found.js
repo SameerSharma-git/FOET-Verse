7fa81efc75f8ecd66c7e39e87837153dff7d57e6
SameerSharma-git <sameersharm1234@gmail.com>
2026-01-04 14:50:20 +0530

    Adjust height of NotFound component

--- app/not-found.js
+++ app/not-found.js
@@ -4,7 +4,7 @@ import { ArrowRight } from 'lucide-react';
 
 export default function NotFound() {
     return (
-        <div className='w-full h-[65vh] md:h-[80vh] flex items-center justify-center gap-6 flex-col'>
+        <div className='w-full h-[65vh] md:h-[88vh] flex items-center justify-center gap-6 flex-col'>
             <div className='w-full h-1/2 bg-no-repeat bg-center mx-auto bg-[url("/images/not_found_dog-98fd5bff8642dd0089c45b3a82fe5d56.svg")]'>
             </div>
             <div className='flex items-center justify-center gap-3.5 flex-col'>
